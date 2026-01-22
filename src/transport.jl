a076ddd7f3c2907c64eeb40fe3500e07648c809b
Erin Atkinson <erin.atkinson@mail.utoronto.ca>
2026-01-22 13:27:48 -0500

    Remove bonkers third figure

--- src/transport.jl
+++ src/transport.jl
@@ -33,12 +33,6 @@ ax_w′c′ = Axis(fig[1, 2];
     limits = (nothing, nothing, nothing, nothing)
 )
 
-ax_w′c′_Ri = Axis(fig[1, 3]; 
-    ylabel = L"\int_0^t \langle w'c' \rangle \text{d}t/ \text{m}",
-    xlabel = L"\text{Ri}_b",
-    limits = (nothing, nothing, nothing, nothing)
-)
-
 lns = map(datasets) do dataset
     t = dataset.Ri_b.times / 3600
     Ri_b = [dataset.Ri_b[n][1] for n in 1:length(t)]
@@ -46,12 +40,11 @@ lns = map(datasets) do dataset
     w′c′ = cumsum(w′c′) * (t[2] - t[1])
     lines!(ax_Ri, t, Ri_b)
     lines!(ax_w′c′, t, w′c′)
-    lines!(ax_w′c′_Ri, Ri_b, w′c′)
 end
 
 #labels = ["$(round(p.Ri; digits=1))" for p in parameters]
 labels = ["$(round(Ri; digits=1))" for Ri in [0.3, 0.5, 0.7, 0.9]]
-Legend(fig[1, 4], lns, labels, L"\text{Ri}")
+Legend(fig[1, 3], lns, labels, L"\text{Ri}")
 
 save("images/transport.png", fig; px_per_unit=2)
 fig
